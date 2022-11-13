a3ab12ec1559bd6e4e8871ca0b29ff221206856e
Bedin Andriy <bulgar198605@gmail.com>
2022-11-13 18:45:09 +0200

    13--11

--- src/slices/userSlice.js
+++ src/slices/userSlice.js
@@ -38,7 +38,21 @@ const userSlice = createSlice({
     changeUserStorage: (state, action) => {
       const getLocalS = JSON.parse(localStorage.getItem('users'));
 
-     
+      for (let i = 0; i < getLocalS.data.length; i++) {
+        if (getLocalS.data[i].id == action.payload.id) {
+          getLocalS.data[i].name = action.payload.lastName;
+          getLocalS.data[i].email = action.payload.email;
+          getLocalS.data[i].phone = action.payload.phone;
+          getLocalS.data[i].company.name = action.payload.companyName;
+          getLocalS.data[i].company.catchPhrase = action.payload.specialization;
+          getLocalS.data[i].website = action.payload.website;
+          getLocalS.data[i].company.bs = action.payload.tagline;
+          getLocalS.data[i].address.city = action.payload.city;
+          getLocalS.data[i].address.street = action.payload.street;
+
+          state.storLocalUser = getLocalS.data[i];
+        }
+      }
       localStorage.setItem('users', JSON.stringify(getLocalS));
     },
     stateUser: (state, action) => {
@@ -46,9 +60,13 @@ const userSlice = createSlice({
       const arr = JSON.parse(localStorage.getItem('users')).data[
         action.payload - 1
       ];
-     
+      state.storLocalUser = arr;
+      state.userArr = arrA;
     },
-   
+    allRemove: (state) => {
+      state.userArr = [];
+      localStorage.clear();
+    },
   },
   extraReducers: {
     [getUsers.pending]: (state) => {
@@ -56,7 +74,11 @@ const userSlice = createSlice({
       state.status = 'loading';
       state.error = null;
     },
-   
+    [getUsers.fulfilled]: (state, action) => {
+      console.log('getUsers: fulfilled');
+      state.status = 'fulfilled';
+      state.userArr = action.payload;
+    },
     [getUsers.rejected]: setError,
   },
 });
